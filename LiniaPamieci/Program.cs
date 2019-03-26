8064dde2e046609e597bf55b28e5b5e910ea80c2
Kamil Stolarczyk <kstolarczyk@elewatorsoft.onmicrosoft.com>
2019-03-26 15:21:56 +0100

    dag

--- LiniaPamieci/Program.cs
+++ LiniaPamieci/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,52 +15,68 @@ namespace LiniaPamieci
         private static void licz(object data)
         {
             Parameters p = (Parameters)data;
-            for(int i = 0; i < 20000000; i++)
+            
+            for(int i = 0; i < 5000000; i++)
             {
                 p.tab[p.indeks] += 1;
             }
         }
+        private static int[] s_counter = new int[1024];
+        private static void UpdateCounter(object data)
+        {
+            Parameters p = (Parameters)data;
+            Stopwatch s = new Stopwatch();
+            s.Start();
+
+            for (int j = 0; j < 100000000; j++)
+            {
+                s_counter[p.indeks] = s_counter[p.indeks] + 3;
+            }
+
+            s.Stop();
+            Console.WriteLine("Thread{0} time: {1}ms", Thread.CurrentThread.ManagedThreadId, s.ElapsedMilliseconds);
+        }
         static void Main(string[] args)
         {
-            int[] tab = new int[256];
-            watch = new Stopwatch();
-            Thread[] threads = new Thread[2];
-            Parameters[] param = new Parameters[2];
-            int first = 0;
-            int last = 63;
-            Thread.Sleep(500);
-            while(last >= 7)
+            Stopwatch watch = new Stopwatch();
+            int first = 8;
+            int second = 24;
+            int third = 40;
+            int last = 56;
+            Parameters param1 = new Parameters(ref s_counter, first);
+            Parameters param2 = new Parameters(ref s_counter, second);
+            Parameters param3 = new Parameters(ref s_counter, third);
+            Parameters param4 = new Parameters(ref s_counter, last);
+            double avg = 0;
+            for (int i = 0; i < 3; i++)
             {
-                double avg = 0;
-                param[0] = new Parameters(ref tab, first);
-                param[1] = new Parameters(ref tab, last);
-                for (int i = 0; i < 100; i++)
-                {
-                    threads[0] = new Thread(new ParameterizedThreadStart(licz));
-                    threads[1] = new Thread(new ParameterizedThreadStart(licz));
-                    watch.Start();
-                    threads[0].Start(param[0]);
-                    threads[1].Start(param[1]);
-                    threads[0].Join();
-                    threads[1].Join();
-                    watch.Stop();
-                    avg += watch.ElapsedMilliseconds;
-                    watch.Reset();
-                }
-                Console.WriteLine("Time elapsed: {0}ms", avg / 100);
-                Console.WriteLine("tab[{0}] = {1}, tab[{2}] = {3}", first, tab[first], last, tab[last]);
-                tab[first] = 0;
-                last -= 8;
+                Thread thread1 = new Thread(new ParameterizedThreadStart(UpdateCounter));
+                Thread thread2 = new Thread(new ParameterizedThreadStart(UpdateCounter));
+                Thread thread3 = new Thread(new ParameterizedThreadStart(UpdateCounter));
+                Thread thread4 = new Thread(new ParameterizedThreadStart(UpdateCounter));
+                watch.Start();
+                thread1.Start(param1);
+                thread2.Start(param2);
+                thread3.Start(param3);
+                thread4.Start(param4);
+                thread1.Join();
+                thread2.Join();
+                thread3.Join();
+                thread4.Join();
+                watch.Stop();
+                avg += watch.ElapsedMilliseconds;
+                s_counter[first] = 0;
+                s_counter[second] = 0;
+                s_counter[third] = 0;
+                s_counter[last] = 0;
+                watch.Reset();
             }
-            //unsafe
-            //{
-            //    fixed(int* pointer = &tab[0])
-            //    {
-            //        Console.WriteLine("Adresy: {0}, {1}, {2}, {3}", (int)pointer, (int)pointer + 1, (int)pointer + 2, (int)pointer + 3);
-            //        Console.WriteLine("Wartości {0}, {1}, {2}, {3}", *pointer, *(pointer + 1), *(pointer + 2), *(pointer + 3));
-            //    }
-
-            //}
+            unsafe
+            {
+                Console.WriteLine("size of pointer: {0} bytes", sizeof(int*));
+                Console.WriteLine("size of int: {0} bytes", sizeof(int));
+            }
+            Console.WriteLine("Average time: {0}ms", avg / 3);
             Console.WriteLine("Koniec");
             Console.ReadKey();
         }
