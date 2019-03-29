0504eeb5efd1009fe67ef868cceecdf09a529c97
Kamil Stolarczyk <kstolarczyk@elewatorsoft.onmicrosoft.com>
2019-03-29 09:33:39 +0100

    poprawione

--- LiniaPamieci/Program.cs
+++ LiniaPamieci/Program.cs
@@ -1,42 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace LiniaPamieci
 {
     class Program
     {
+
         unsafe private static void licz(object data)
         {
             Parameters p = (Parameters)data;
-            
-            for(int i = 0; i < 5000000; i++)
+            for (int i = 0; i < 1000000; i++)
             {
                 p.tab[p.indeks] += 3;
             }
         }
-       
+
         unsafe static void Main(string[] args)
         {
+            Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.RealTime;
             Stopwatch watch = new Stopwatch();
             int* tab = (int*)Marshal.AllocHGlobal(sizeof(int) * 256);
             int maxHopes = 32;
-            int cacheLineSize = sizeof(int)*maxHopes;
+            int cacheLineSize = sizeof(int) * maxHopes;
             double currentSum = 0;
             int counter = 0;
             Console.WriteLine("Sizeof element: {0} bytes, sizeof pointer: {1} bytes", sizeof(int), sizeof(int*));
             for (int i = 0; i < maxHopes; i++)
             {
-                double currentAvg = currentSum / (i>0 ? i : 1);
+                double currentAvg = currentSum / (i > 0 ? i : 1);
                 Parameters p1 = new Parameters(tab, i);
                 Parameters p2 = new Parameters(tab, i + 1);
                 double avg = 0;
-                for(int k = 0; k < 10; k++)
+                for (int k = 0; k < 20; k++)
                 {
                     Thread t1 = new Thread(new ParameterizedThreadStart(licz));
                     Thread t2 = new Thread(new ParameterizedThreadStart(licz));
@@ -51,9 +48,8 @@ namespace LiniaPamieci
                     avg += watch.ElapsedMilliseconds;
                     watch.Reset();
                 }
-                avg /= 10;
-                
-                if(i > 0 && Math.Abs(avg - currentAvg) > 0.5*currentAvg)
+                avg /= 20;
+                if (i > 0 && Math.Abs(avg - currentAvg) > (0.4 * currentAvg))
                 {
                     counter++;
                     currentSum += currentAvg;
@@ -63,11 +59,19 @@ namespace LiniaPamieci
                     currentSum += avg;
                 }
 
-                int *p = &tab[i];
-                Console.WriteLine("Threads operate at indexes: {0} and {1} in average time: {2}ms, elements adresses: {3}, {4}", i, i + 1, avg, (int)p, (int)(p+1));
-                
+                int* p = &tab[i];
+                Console.WriteLine("Threads operate at indexes: {0} and {1} in average time: {2}ms\t|\telements adresses: {3}, {4}", i, i + 1, avg, (int)p, (int)(p + 1));
+
             }
-            Console.WriteLine("Cache line size is: {0} bytes", (cacheLineSize/counter));
+            Marshal.FreeHGlobal((IntPtr)tab);
+            if (counter == 0)
+            {
+                Console.WriteLine("Nie udało się obliczyć długości linii pamięci. Wyłącz kurwa optymalizacje kodu albo wypierdol ten komputer");
+            }
+            else
+            {
+                Console.WriteLine("Cache line size is: {0} bytes", (cacheLineSize / counter));
+            }
             Console.ReadKey();
         }
     }
